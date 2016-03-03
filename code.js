4394ecc0d64e3fa86061a13ba24cd8b5bf6abb91
Pavel Boldin <boldin.pavel@gmail.com>
2016-03-03 16:15:44 +0200

    moving new code over old one

--- code.js
+++ code.js
@@ -1,194 +1,228 @@
 
-var debug = true;
-var debug_iterState = true;
-var DEBUG = function(){console.log.apply(console, arguments)};
-
-if (!debug)
-  DEBUG = function(){};
-
 function TreeGenerator(crl, lcr) {
     if (crl.length != lcr.length)
         throw ("Invalid arg lengths: crl.length = " + crl.length +
                " vs lcr.length = " + lcr.length);
 
-    var stateObj = [];
-    var currentLevel = -1;
+    var newNode = function(state, value) {
+        if (state === null)
+            return null;
 
-    this._printState = function(state) {
-      if (state === null) {
-        console.log('state is null');
-        return;
-      }
-      console.log('state.lcr', state.lcr);
-      var lcr_ = lcr.substring(state.lcr.left, state.lcr.right + 1);
-      console.log('lcr', lcr_);
-      if (state.lcr.center >= state.lcr.left) {
-        var center = '-'.repeat(state.lcr.center - state.lcr.left) + '^' +
-                     '-'.repeat(state.lcr.right - state.lcr.center);
-        console.log('lcr', center);
-      }
+        if (state.lcr.left == state.lcr.right)
+            return new NodeStatic(state);
 
-      console.log('state.crl', state.crl);
-      console.log('crl', crl.substring(state.crl.left, state.crl.right + 1));
-      if (state.crl.center >= state.crl.left) {
-        var center = '-'.repeat(state.crl.center - state.crl.left) + '^' +
-                     '-'.repeat(state.crl.right - state.crl.center);
-        console.log('crl', center);
-      }
+        return new NodeIterator(state);
     };
 
+    var NodeStatic = function(state) {
+        if (state.crl.left != state.crl.right)
+            throw "Not a static node";
 
-    this._checkState = function(state) {
-      if (state.lcr.left > state.lcr.right)
-        return null;
-      return state;
+
+        var center = crl[state.crl.left];
+        var shown = false;
+
+        this.reset = function() {
+            shown = false;
+        };
+
+        this.next = function() {
+            if (shown)
+                return;
+
+            shown = true;
+            return {
+                center: center
+            };
+        };
     };
 
+    var NodeIterator = function(state) {
+        var centerSymbol = crl[state.crl.left];
+        var splitPosition = state.lcr.left - 1;
+        var crlSplitPosition = -1;
+        var left = null, right = null, leftIterator, rightIterator;
 
-    this._iterState = function(parentState) {
-      var centerSymbol = crl[parentState.crl.left];
-      var center = lcr.indexOf(centerSymbol, parentState.lcr.center + 1);
+        var _print = this.print = function(logPrefix) {
+            var log = console.log.bind(console);
+            if (logPrefix !== undefined && logPrefix !== '')  {
+                log = console.log.bind(console, "L" + logPrefix);
+            }
 
-      if (center < 0 || center > parentState.lcr.right) {
-        return;
-      }
+            log('state.lcr', {left: state.lcr.left, right: state.lcr.right,
+                              center: splitPosition});
+            var lcr_ = lcr.substring(state.lcr.left, state.lcr.right + 1);
+            log('lcr', "'" + lcr_ + "'");
+            if (splitPosition >= state.lcr.left
+                && splitPosition <= state.lcr.right) {
+              var center = '-'.repeat(splitPosition - state.lcr.left) + '^' +
+                           '-'.repeat(state.lcr.right - splitPosition);
+              log('lcr', "'" + center + "'");
+            }
 
-      var crlCenter = parentState.lcr.right - center;
+            log('state.crl', state.crl);
+            log('crl', "'" + crl.substring(state.crl.left, state.crl.right + 1) + "'");
+            if (crlSplitPosition >= state.crl.left) {
+              var center = '-'.repeat(crlSplitPosition - state.crl.left) + '^' +
+                           '-'.repeat(state.crl.right - crlSplitPosition);
+              log('crl', "'" + center + "'");
+            }
+        };
 
-      var newState = {
+        this.reset = function() {
+            splitPosition = state.lcr.left - 1;
+            crlSplitPosition = -1;
+            leftIterator = rightIterator = null;
+        };
+
+        var set = function(s) {
+            return s.split('').sort().join('');
+        };
+
+        var checkState = function(state) {
+            if (state.lcr.left > state.lcr.right)
+                return null;
+
+            var lcrSet = lcr.substring(state.lcr.left, state.lcr.right + 1)
+            var crlSet = crl.substring(state.crl.left, state.crl.right + 1);
+
+            if (set(lcrSet) != set(crlSet))
+                return false;
+
+            return state;
+        };
+
+        var nextSplitPosition = function(){
+
+            splitPosition = lcr.indexOf(centerSymbol, splitPosition + 1);
+            crlSplitPosition = -1;
+
+            if (splitPosition < 0 || splitPosition > state.lcr.right)
+                return false;
+
+
+            crlSplitPosition = state.crl.left + state.lcr.right - splitPosition;
+            return true;
+        };
+
+        var nextIterators = function() {
+
+            var leftState = {
+                lcr: {
+                    left: state.lcr.left,
+                    right: splitPosition - 1,
+                },
+                crl: {
+                    left: 1 + crlSplitPosition,
+                    right: state.crl.right,
+                },
+            };
+
+            var rightState = {
+                lcr: {
+                    left: splitPosition + 1,
+                    right: state.lcr.right,
+                },
+                crl: {
+                    left: 1 + state.crl.left,
+                    right: crlSplitPosition,
+                },
+            };
+
+            leftState = checkState(leftState);
+            rightState = checkState(rightState);
+
+            if (leftState === false || rightState === false)
+                return false;
+
+            leftIterator = newNode(leftState);
+            rightIterator = newNode(rightState);
+
+            return true;
+        };
+
+        var nextSubtree = function(l) {
+            var newSubtree = false;
+ 
+            leftIterator = rightIterator = null;
+
+            while (nextSplitPosition())
+                if (newSubtree = nextIterators())
+                    break;
+
+            return newSubtree;
+        };
+
+        var nextLeft = function(l) {
+            left = null;
+            if (leftIterator) {
+                left = leftIterator.next(2*l + 1);
+                if (left !== undefined)
+                    return true;
+            }
+
+            if (!nextSubtree(l))
+                return false;
+
+            left = null;
+            if (leftIterator) {
+                left = leftIterator.next(2*l + 1);
+            }
+            return true;
+        };
+
+        var nextRight = function(l) {
+            right = null;
+            if (rightIterator) {
+                right = rightIterator.next(2*l + 2);
+                if (right !== undefined)
+                    return true;
+            }
+
+            if (!nextLeft(l))
+                return false;
+
+            right = null;
+            if (rightIterator) {
+                rightIterator.reset();
+                right = rightIterator.next(2*l + 2);
+            }
+            return true;
+        };
+
+        this.next = function(l) {
+            do {
+                if (!nextRight(l)) {
+                    return;
+                }
+                /* undefined left or right means that sibling's .next()
+                   failed to iterate due to impossible split
+                 */
+            } while (left === undefined || right === undefined);
+
+            return {
+                center: centerSymbol,
+                left: left,
+                right: right
+            };
+        };
+
+    };
+
+    var rootState = {
         lcr: {
-          left: parentState.lcr.left,
-          center: center,
-          right: parentState.lcr.right,
+           left: 0,
+           right: lcr.length - 1,
         },
         crl: {
-          left: parentState.crl.left,
-          center: parentState.crl.left + crlCenter,
-          right: parentState.crl.right,
+           left: 0,
+           right: crl.length - 1,
         },
-      };
-
-
-      var leftState = {
-        lcr: {
-          left: parentState.lcr.left,
-          center: parentState.lcr.left - 1,
-          right: center - 1,
-        },
-        crl: {
-          left: 1 + parentState.crl.left + crlCenter, /* skip the 'centerSymbol' itself */
-          right: parentState.crl.right,
-        },
-      };
-
-      var rightState = {
-        lcr: {
-          left: center + 1,
-          center: center,
-          right: parentState.lcr.right,
-        },
-        crl: {
-          left: 1 + parentState.crl.left, /* skip the 'centerSymbol' itself */
-          right: parentState.crl.left + crlCenter,
-        },
-      };
-
-      leftState = this._checkState(leftState);
-      rightState = this._checkState(rightState);
-
-      if (debug_iterState) {
-          console.log('new');
-          this._printState(newState);
-          console.log('left');
-          this._printState(leftState);
-          console.log('right');
-          this._printState(rightState);
-      }
-
-      return [newState, leftState, rightState];
     };
+    var rootIterator = newNode(rootState);
 
-    this._startTree = function() {
-      stateObj[0] = {
-        lcr: {
-          left: 0,
-          center: -1,
-          right: lcr.length - 1,
-        },
-        crl: {
-          left: 0,
-          right: crl.length - 1,
-        },
-      };
-      currentLevel = 0;
-
-      while(currentLevel >= 0 && currentLevel < stateObj.length) {
-        var levels = Object.keys(stateObj).map(
-                            function(e){return +e})
-                        .filter(
-                            function(a){ return a > currentLevel})
-                        .sort(
-                            function(a, b){ return a - b; });
-        var nextLevel = levels[0] || (currentLevel + 1);
-        console.log(nextLevel, currentLevel, levels);
-
-        if (!stateObj[currentLevel]) {
-          currentLevel = nextLevel;
-          continue;
-        }
-
-        var result = this._iterState(stateObj[currentLevel]);
-        if (result === undefined) {
-          console.log('failed state');
-          this._printState(stateObj[currentLevel]);
-          /* Delete childs */
-          delete stateObj[2*currentLevel + 1];
-          delete stateObj[2*currentLevel + 2];
-          currentLevel = Math.floor((currentLevel - 1) / 2);
-          /* Delete self and sibling as well */
-          delete stateObj[2*currentLevel + 1];
-          delete stateObj[2*currentLevel + 2];
-          continue;
-        }
-          
-        stateObj[currentLevel] = result[0];
-
-        if (result[1])
-          stateObj[2 * currentLevel + 1] = result[1]; /* left */
-        else
-          delete stateObj[2 * currentLevel + 1];
-
-        if (result[2])
-          stateObj[2 * currentLevel + 2] = result[2]; /* left */
-        else
-          delete stateObj[2 * currentLevel + 2];
-
-        currentLevel = nextLevel;
-      }
+    this.iterateTree = function(){
+        var r = rootIterator.next(0);
+        return r;
     };
-
-    this.iterateTree = function() {
-      if (currentLevel == -1) {
-        this._startTree();
-      }
-      else {
-        this._iterTree();
-      }
-
-      return this._treeFromStateHeap(stateObj, 0);
-    };
-
-    this._treeFromStateHeap = function(stateObj, i) {
-      i = i || 0;
-      if (!stateObj[i])
-        return null;
-      var center = lcr[stateObj[i].lcr.center];
-      console.log(i, stateObj[i].lcr, center);
-
-      return {
-        center: center,
-        left: this._treeFromStateHeap(stateObj, i*2 + 1),
-        right: this._treeFromStateHeap(stateObj, i*2 + 2),
-      }
-    };
-};
+}
